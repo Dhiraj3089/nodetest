938035b01c4f2a8cfc759aea5ae27da30571fb56
nIDO_3089 <dhiraj.katekar@gmail.com>
2017-10-03 12:14:11 +0530

    test

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mysqlURL = process.env.OPENSHIFT_MYSQL_DB_URL || process.env.MYSQL_URL,
+    mongoURLLabel = "";
+  res.send('Hello World!' + mysqlURL);
 })
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
