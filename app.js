ab016d40e992b7057965b3eeb8e183667b8ac629
nIDO_3089 <dhiraj.katekar@gmail.com>
2017-10-03 12:53:04 +0530

    test

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ app.get('/', function (req, res) {
   var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mysqlURL = process.env.OPENSHIFT_MYSQL_DB_URL || process.env.MYSQL_URL,
+    mysqlUSER = process.env.OPENSHIFT_MYSQL_USER || process.env.MYSQL_USER,
     mongoURLLabel = "";
-  res.send('Hello World!' + mysqlURL);
+  res.send('Hello World!' + mysqlUSER);
 })
 
 app.listen(8080, function () {
